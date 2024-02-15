20da5ca0ffd880bca7f30d77d5c1bf4bf81104d0
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-02-15 09:52:56 -0300

    App

---
+++ Form.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd 
+
+base = pd.read_excel('./BASE ALUNOS.xlsx')
+
+id_func = st.selectbox('Qual seu ID ?', base['ID'].unique())
+
+
+base_fil = base[base['ID'] == id_func]
+
+st.markdown(f'Olá {base_fil.pop('NOME').iloc[0]}!')
+
+if base_fil['RESPOSTA'].notnull().any():
+    st.write('Você já respondeu esse formulário!')
+
+else:
+    with st.form("formulario_perguntas"):
+        respostas = {}
+
+        for index, row in base_fil.iterrows():
+            # Criar botões de rádio para cada pergunta
+            resposta = st.radio(row['PERGUNTA'], ['Sim', 'Não'], key=index)
+            respostas[row['PERGUNTA']] = resposta
+            
+        submitted = st.form_submit_button("Enviar")
+        if submitted:
+            # Processar as respostas
+            for pergunta, resposta in respostas.items():
+                st.write("Resposta registrada com sucesso!")
+                indices = base[base['ID'] == id_func].index
+                for indice in indices:
+                    base.at[indice, 'RESPOSTA'] = resposta
+
+base.to_excel('./BASE ALUNOS.xlsx', index=False)
+
+
