b3fa526ef278a466061541d083bc58ef1d3f98b5
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-03-19 19:16:00 +0000

    as

--- Form.py
+++ Form.py
@@ -6,7 +6,7 @@ base = pd.read_excel('./BASE ALUNOS.xlsx')
 id_func = st.selectbox('Qual seu ID ?', base['ID'].unique())
 
 
-st.write(st.secrets['db_username'])
+st.write(st.secrets['db_username'][0])
 st.write(st.secrets['things_i_like'])
 
 
